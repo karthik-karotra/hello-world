66830f3131d596a0b6cd5ca53d1e477e1f35f211
karthik-karotra <karthikpatel54@gmail.com>
2022-11-25 21:58:25 +0530

    New Branch Testing

--- src/test/hello/HelloWorldGitDemo.java
+++ src/test/hello/HelloWorldGitDemo.java
@@ -4,5 +4,7 @@ public class HelloWorldGitDemo {
     public static void main(String[] args) {
         System.out.println("Welcome");
         System.out.println("hii");
+        System.out.println("Why to learn Git");
+        System.out.println("I am comfortable with Git");
     }
 }
